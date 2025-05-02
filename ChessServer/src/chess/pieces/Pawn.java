ae8162492e818f521822371c565ea505d691a287
Guido Michieletto <guido@michieletto.it>
2025-05-02 23:48:06 +0200

    fix su movimento pedone

--- ChessServer/src/chess/pieces/Pawn.java
+++ ChessServer/src/chess/pieces/Pawn.java
@@ -15,12 +15,32 @@ public class Pawn extends Piece {
         ArrayList<Point> availableMoves = new ArrayList<>();
 
         // White
-        if(this.getColor() == Color.WHITE && start.y == 6 && this.getGame().getPiece(start.x, start.y - 2) == null) availableMoves.add(new Point(start.x, start.y - 2));
-        if(this.getColor() == Color.WHITE && start.y - 1 >= 0 && this.getGame().getPiece(start.x, start.y - 1) == null) availableMoves.add(new Point(start.x, start.y - 1));
+        if(this.getColor() == Color.WHITE) {
+            if(start.y == 6 && this.getGame().getPiece(start.x, start.y - 2) == null) availableMoves.add(new Point(start.x, start.y - 2));
+            if(start.y - 1 >= 0 && this.getGame().getPiece(start.x, start.y - 1) == null) availableMoves.add(new Point(start.x, start.y - 1));
+
+            if(start.y - 1 >= 0 && start.x + 1 < Game.COLS && this.getGame().getPiece(start.x + 1, start.y - 1) != null && this.getGame().getPiece(start.x + 1, start.y - 1).getColor() != this.getColor()) {
+                availableMoves.add(new Point(start.x + 1, start.y - 1));
+            }
+            if(start.y - 1 >= 0 && start.x - 1 >= 0 && this.getGame().getPiece(start.x - 1, start.y - 1) != null && this.getGame().getPiece(start.x - 1, start.y - 1).getColor() != this.getColor()) {
+                availableMoves.add(new Point(start.x - 1, start.y - 1));
+            }
+        }
+
 
         // Black
-        if(this.getColor() == Color.BLACK && start.y == 1 && this.getGame().getPiece(start.x, start.y + 2) == null) availableMoves.add(new Point(start.x, start.y + 2));
-        if(this.getColor() == Color.BLACK && start.y + 1 < Game.ROWS && this.getGame().getPiece(start.x, start.y + 1) == null) availableMoves.add(new Point(start.x, start.y + 1));
+        if(this.getColor() == Color.BLACK) {
+            if(start.y == 1 && this.getGame().getPiece(start.x, start.y + 2) == null) availableMoves.add(new Point(start.x, start.y + 2));
+            if(start.y + 1 < Game.ROWS && this.getGame().getPiece(start.x, start.y + 1) == null) availableMoves.add(new Point(start.x, start.y + 1));
+
+            if(start.y + 1 < Game.ROWS && start.x + 1 < Game.COLS && this.getGame().getPiece(start.x + 1, start.y + 1) != null && this.getGame().getPiece(start.x + 1, start.y + 1).getColor() != this.getColor()) {
+                availableMoves.add(new Point(start.x + 1, start.y + 1));
+            }
+            if(start.y + 1 < Game.ROWS && start.x - 1 >= 0 && this.getGame().getPiece(start.x - 1, start.y + 1) != null && this.getGame().getPiece(start.x - 1, start.y + 1).getColor() != this.getColor()) {
+                availableMoves.add(new Point(start.x - 1, start.y + 1));
+            }
+        }
+
 
         return availableMoves;
     }
